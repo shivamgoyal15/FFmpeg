e1c7347afc55624c6ad3831fbeeaae601d698e8e
Shivam Goyal <shivamgoyal1506@outlook.com>
2019-07-21 23:35:42 +0530

    Changed metadata system

--- libavformat/dicomdec.c
+++ libavformat/dicomdec.c
@@ -30,18 +30,14 @@
 
 
 typedef struct DICOMContext {
-    const AVClass *class; ///< Class for private options.
-    int big_endian;
+    const AVClass *class;
 
     uint64_t nb_frames;
-    uint16_t bits_allocated;
-    uint16_t bits_stored;
-    uint16_t high_bit;
-    uint16_t rows;
-    uint16_t columns;
-    uint32_t nb_DEs;
+    uint16_t width;
+    uint16_t height;
     TransferSyntax transfer_syntax;
-    int window;
+
+    int window; // Options
     int level;
     int metadata;
 } DICOMContext;
@@ -71,29 +67,24 @@ static int set_imagegroup_data(AVFormatContext *s, AVStream* st, DataElement *de
         return 0;
 
     switch (de->ElementNumber) {
-        case 0x0010:
-            dicom->rows = *(uint16_t *)bytes;
-            st->codecpar->height = dicom->rows;
+        case 0x0010: // rows
+            dicom->height = *(uint16_t *)bytes;
+            st->codecpar->height = dicom->height;
             break;
-        case 0x0011:
-            dicom->columns = *(uint16_t *)bytes;
-            st->codecpar->width = dicom->columns;
+        case 0x0011: // columns
+            dicom->width = *(uint16_t *)bytes;
+            st->codecpar->width = dicom->width;
             break;
-        case 0x0100:
-            dicom->bits_allocated = *(uint16_t *)bytes;
-            st->codecpar->bits_per_raw_sample = dicom->bits_allocated;
+        case 0x0100: // bits allocated
+            st->codecpar->bits_per_raw_sample = *(uint16_t *)bytes;
             break;
-        case 0x0101:
-            dicom->bits_stored = *(uint16_t *)bytes;
-            st->codecpar->bits_per_coded_sample = dicom->bits_stored;
+        case 0x0101: // bits stored
+            st->codecpar->bits_per_coded_sample = *(uint16_t *)bytes;
             break;
-        case 0x0102:
-            dicom->high_bit = *(uint16_t *)bytes;
-            break;
-        case 0x0008:
+        case 0x0008: // number of frames
             dicom->nb_frames = *(uint32_t *)bytes;
             break;
-        case 0x1050:
+        case 0x1050: // level
             if (dicom->level == -1) {
                 cbytes = av_malloc(len + 1);
                 memcpy(cbytes, bytes, len);
@@ -103,13 +94,13 @@ static int set_imagegroup_data(AVFormatContext *s, AVStream* st, DataElement *de
                 av_free(cbytes);
             }
             break;
-        case 0x1051:
+        case 0x1051: // window
             if (dicom->window == -1) {
                 cbytes = av_malloc(len + 1);
                 memcpy(cbytes, bytes, len);
                 cbytes[len] = 0;
                 st->codecpar->profile = atoi(cbytes);
-                dicom->level = st->codecpar->profile;
+                dicom->window = st->codecpar->profile;
                 av_free(cbytes);
             }
             break;
@@ -121,6 +112,7 @@ static int set_imagegroup_data(AVFormatContext *s, AVStream* st, DataElement *de
 
 static int read_data_element_metainfo(AVFormatContext *s, DataElement *de)
 {
+    DICOMContext *dicom = s->priv_data;
     ValueRepresentation vr;
     int ret;
     int bytes_read = 0;
@@ -129,9 +121,12 @@ static int read_data_element_metainfo(AVFormatContext *s, DataElement *de)
     if (ret < 0)
         return ret;
     de->GroupNumber = ret;
-
     de->ElementNumber =  avio_rl16(s->pb);
-
+    if (dicom->transfer_syntax == IMPLICIT_VR) { // if implicit no need to find VR
+        de->VL = avio_rl32(s->pb);
+        bytes_read += 8;
+        return bytes_read;
+    }
     vr = avio_rb16(s->pb); // Stored in Big Endian in dicom.h
     de->VR = vr;
     bytes_read += 6;
@@ -169,53 +164,76 @@ static int read_data_element_valuefield(AVFormatContext *s, DataElement *de)
 {
     int ret;
     uint32_t len = de->VL;
-
     de->bytes = av_malloc(len);
     ret = avio_read(s->pb, de->bytes, len);
     return ret;
-}
-
-static int print_data_element_metadata(AVFormatContext *s, DataElement *de) {
-    av_log(s, AV_LOG_INFO,"(%04x, %04x)\tVR=%c%c\tVL=%d\t",de->GroupNumber, de->ElementNumber, de->VR>>8, de->VR - ((de->VR>>8)<<8), de->VL);
-    return 0;
-}
-
-static int print_data_element_value(AVFormatContext *s, DataElement *de) {
-    ValueRepresentation vr = de->VR;
-    uint32_t len = de->VL;
-    void *data = de->bytes;
-    char *cdata;
-
-    switch (vr) {
-        case AT:
-            if (len < 4) {
-                printf("[Invalid]\n");
-                return 0;
-            }
-            cdata = data;
-            av_log(s, AV_LOG_INFO, "%04d %04d", cdata[1] << 8 + cdata[0], cdata[4] << 8 + cdata[3]);
-            break;
-        case FL:
-            av_log(s, AV_LOG_INFO, "%f\n", ((float *)data)[0]);
-            break;
-        default:
-            cdata = av_malloc(len + 1);
-            memcpy(cdata, data, len);
-            cdata[len] = 0;
-            av_log(s, AV_LOG_INFO, "%s\n", cdata);
-            av_free(cdata);
-    }
-    return 0;
 }
 
 static char *get_key_str(DataElement *de) {
     char *key;
     if (!de->GroupNumber || !de->ElementNumber)
         return 0;
-    key = (char *) av_malloc(10);
-    sprintf(key, "%04x,%04x", de->GroupNumber, de->ElementNumber);
-    key[9] = 0;
+    key = (char *) av_malloc(15 + strlen(de->desc));
+    sprintf(key, "(%04x,%04x) %s", de->GroupNumber, de->ElementNumber, de->desc);
     return key;
+}
+
+static char *get_val_str(DataElement *de) {
+    char *val;
+    
+    switch(de->VR) {
+        case AT:
+        case OB:
+        case OD:
+        case OF:
+        case OL:
+        case OV:
+        case OW:
+            val = "[Binary data]";
+            break;
+        case UN:
+        case SQ:
+            val = "[Sequence of items]";
+            break;
+        case FL:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%f", ((float *)de->bytes)[0]);
+            break;
+        case FD:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%lf", ((double *)de->bytes)[0]);
+            break;
+        case SL:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%d", ((int32_t *)de->bytes)[0]);
+            break;
+        case SS:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%d", ((int16_t *)de->bytes)[0]);
+            break;
+        case SV:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%ld", ((int64_t *)de->bytes)[0]);
+            break;
+        case UL:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%ud", ((uint32_t *)de->bytes)[0]);
+            break;
+        case US:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%ud", ((uint16_t *)de->bytes)[0]);
+            break;
+        case UV:
+            val = (char *) av_malloc(10);
+            sprintf(val, "%lud", ((uint64_t *)de->bytes)[0]);
+            break;
+        default:
+            val = (char *) av_malloc(de->VL + 1);
+            memcpy(val, de->bytes, de->VL);
+            val[de->VL] = 0;
+            break;
+    }
+    return val;
 }
 
 // detects transfer syntax
@@ -260,15 +278,13 @@ static int dicom_read_header(AVFormatContext *s)
         if (ret < 0)
             return ret;
         bytes_read += ret;
+        dicom_dict_find_elem_info(de);
         key = get_key_str(de);
         ret = read_data_element_valuefield(s, de);
         if (ret < 0)
             return ret;
         bytes_read += ret;
-        value = (char *) de->bytes;
-        value = av_malloc(de->VL + 1);
-        memcpy(value, de->bytes, de->VL);
-        value[de->VL] = 0;
+        value = get_val_str(de);
         if (de->GroupNumber == TS_GR_NB && de->ElementNumber == TS_EL_NB) {
             dicom->transfer_syntax = get_transfer_sytax(value);
         }
@@ -307,7 +323,7 @@ static int dicom_read_packet(AVFormatContext *s, AVPacket *pkt)
         ret = avio_feof(s->pb);
         if (ret)
             return AVERROR_EOF;
-        
+
         de = av_malloc(sizeof(DataElement));
         ret = read_data_element_metainfo(s,de);
         if (ret < 0)
@@ -333,10 +349,9 @@ static int dicom_read_packet(AVFormatContext *s, AVPacket *pkt)
             ret = read_data_element_valuefield(s, de);
             if (ret < 0)
                 return ret;
-            if (metadata) {
-                print_data_element_metadata(s, de);
-                print_data_element_value(s,de);
-            }
+        }
+        if (metadata) {
+            
         }
         av_free(de);
     }
@@ -344,9 +359,9 @@ static int dicom_read_packet(AVFormatContext *s, AVPacket *pkt)
 }
 
 static const AVOption options[] = {
-    { "window", "window", offsetof(DICOMContext, window), AV_OPT_TYPE_INT, {.i64 = -1}, -1, 99999, AV_OPT_FLAG_DECODING_PARAM },
-    { "level", "level", offsetof(DICOMContext, level), AV_OPT_TYPE_INT, {.i64 = -1}, -1, 99999   , AV_OPT_FLAG_DECODING_PARAM },
-    { "metadata", "Print metadata present in dicom file", offsetof(DICOMContext, metadata)  , AV_OPT_TYPE_BOOL,{.i64 = 0}, 0,1, AV_OPT_FLAG_DECODING_PARAM },
+    { "window", "override default window found in file", offsetof(DICOMContext, window), AV_OPT_TYPE_INT, {.i64 = -1}, -1, 99999, AV_OPT_FLAG_DECODING_PARAM },
+    { "level", "override default level found in file", offsetof(DICOMContext, level), AV_OPT_TYPE_INT, {.i64 = -1}, -1, 99999, AV_OPT_FLAG_DECODING_PARAM },
+    { "metadata", "skip or print metadata", offsetof(DICOMContext, metadata)  , AV_OPT_TYPE_BOOL,{.i64 = 1}, 0, 1, AV_OPT_FLAG_DECODING_PARAM },
     { NULL },
 };
 
